d7cf543c78dd06b8130ef72777d257eb9dc8050d
Kevin McCarthy <kevin.e.mccarthy1983@gmail.com>
2026-05-06 22:11:22 +0200

    commit to commit

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,9 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { NavBar } from "../components/Nav/NavBar";
+import { NavBar } from "../components/Nav/NavBar/NavBar";
 import { ModalProvider } from "../context/ModalProvider";
 import { ToastProvider } from "../context/ToastProvider";
-import { NotFoundPage } from "../pages/ErrorPage";
+import { NotFoundPage } from "../pages/ErrorPage/ErrorPage";
 
 export const Route = createRootRoute({
   component: () => (
